56fefc53fe8195afb9b54d1472a831e4a208543a
Wilfried Hounyo <wilfried.hounyo@gmail.com>
2015-10-24 01:56:19 -0400

    Promisified ajax request helpers

--- main/static/src/script/site/app_extra.js
+++ main/static/src/script/site/app_extra.js
@@ -1,39 +1,43 @@
-function nofunc(){};
 function secondsToString(seconds) {
 	var numminutes = Math.floor((((seconds % 31536000) % 86400) % 3600) / 60);
 	var numseconds = (((seconds % 31536000) % 86400) % 3600) % 60;
 	return numminutes.toFixed(0)+ ":" + ( numseconds < 10 ? "0" : "") + numseconds.toFixed(0);
 };
 function makeAjaxCall (method, p_url, p_data, success_callback, error_callback) {
-    $.ajax({
-      url: p_url,
-      type: method,
-      data: p_data,
-      error: error_callback,
-      success: success_callback,
+	var ajaxRequestPromise = Promise.resolve($.ajax({url: p_url,type: method,data: p_data}));
+    return ajaxRequestPromise.then(function(response) {
+    	if (success_callback != undefined) {
+    		return success_callback(response);
+    	};
+        return response;
+    }, function(error) {
+    	if (error_callback != undefined) {
+    		return error_callback(error);
+    	};
+        return error;
     });
-}
+};
 function getUsingAjax(url, data, success_callback, error_callback) {
 	data = typeof data !== 'undefined' ? data : {};
-	error_callback = typeof error_callback !== 'undefined' ? error_callback : nofunc;
-	success_callback = typeof success_callback !== 'undefined' ? success_callback : nofunc;
-    makeAjaxCall('GET', url, data, success_callback, error_callback);
+	success_callback = typeof success_callback !== 'undefined' ? success_callback : undefined;
+	error_callback = typeof error_callback !== 'undefined' ? error_callback : undefined;
+    return makeAjaxCall('GET', url, data, success_callback, error_callback);
 };
 function postUsingAjax(url, data, success_callback, error_callback) {
 	data = typeof data !== 'undefined' ? data : {};
-	error_callback = typeof error_callback !== 'undefined' ? error_callback : nofunc;
-	success_callback = typeof success_callback !== 'undefined' ? success_callback : nofunc;
-    makeAjaxCall('POST', url, data, success_callback, error_callback);
+	success_callback = typeof success_callback !== 'undefined' ? success_callback : undefined;
+	error_callback = typeof error_callback !== 'undefined' ? error_callback : undefined;
+    return makeAjaxCall('POST', url, data, success_callback, error_callback);
 };
 function putUsingAjax(url, data, success_callback, error_callback) {
 	data = typeof data !== 'undefined' ? data : {};
-	error_callback = typeof error_callback !== 'undefined' ? error_callback : nofunc;
-	success_callback = typeof success_callback !== 'undefined' ? success_callback : nofunc;
-    makeAjaxCall('PUT', url, data, success_callback, error_callback);
+	success_callback = typeof success_callback !== 'undefined' ? success_callback : undefined;
+	error_callback = typeof error_callback !== 'undefined' ? error_callback : undefined;
+    return makeAjaxCall('PUT', url, data, success_callback, error_callback);
 };
 function deleteUsingAjax(url, data, success_callback, error_callback) {
 	data = typeof data !== 'undefined' ? data : {};
-	error_callback = typeof error_callback !== 'undefined' ? error_callback : nofunc;
-	success_callback = typeof success_callback !== 'undefined' ? success_callback : nofunc;
-    makeAjaxCall('DELETE', url, data, success_callback, error_callback);
+	success_callback = typeof success_callback !== 'undefined' ? success_callback : undefined;
+	error_callback = typeof error_callback !== 'undefined' ? error_callback : undefined;
+    return makeAjaxCall('DELETE', url, data, success_callback, error_callback);
 };

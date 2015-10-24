abfb8ad7418e444415cf50351ee3a9c7cda52746
Wilfried Hounyo <wilfried.hounyo@gmail.com>
2015-10-24 03:21:30 -0400

    Added shared for shared controllers

--- main/control/shared.py
+++ main/control/shared.py
@@ -16,11 +16,24 @@ class LessonContentFieldsForm(wtf.Form):
   youtube_video_url = wtforms.StringField('Youtube Video Url', [wtforms.validators.required()])
   vimeo_video_url = wtforms.StringField('Vimeo Video Url', [wtforms.validators.required()])
 
-@app.route('/lesson/commons/fields/<content_type>')
+@app.route('/lesson/commons/fields/<content_type>/')
 def lesson_content_fields(content_type):
   data = flask.json.loads(util.param("data"))
+  form = LessonContentFieldsForm()
   return flask.render_template(
       'shared/lesson/content_fields.html',
+      content_type=content_type,
+      data=data,
+      form=form,
+      html_class='lesson-fields',
+    )
+
+
+@app.route('/lesson/viewport/<content_type>/')
+def lesson_viewport(content_type): 
+  data = flask.json.loads(util.param("data"))
+  return flask.render_template(
+      'shared/viewport.html',
       content_type=content_type,
       data=data,
       html_class='lesson-viewport',

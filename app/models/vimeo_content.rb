c7eec4fca345c1c48a00deeac7a7934818a480c3
Wilfried Hounyo <wilfried.hounyo@gmail.com>
2016-06-04 04:21:15 -0400

    added url validation for vimeo and youtube

--- app/models/vimeo_content.rb
+++ app/models/vimeo_content.rb
@@ -1,3 +1,10 @@
 class VimeoContent < MediaContent
-  validates :video_url, presence: true
+  validates :video_url, presence: true, :format => {:with => /https?:\/\/(?:[\w]+\.)*vimeo\.com(?:[\/\w]*\/?)?\/(?<id>[0-9]+)[^\s]*/i}
+
+  def vimeo_id
+    if self.video_url[/https?:\/\/(?:[\w]+\.)*vimeo\.com(?:[\/\w]*\/?)?\/(?<id>[0-9]+)[^\s]*/]
+      return $1
+    end
+    return
+  end
 end
--- app/models/youtube_content.rb
+++ app/models/youtube_content.rb
@@ -1,3 +1,13 @@
 class YoutubeContent < MediaContent
-  validates :video_url, presence: true
+  validates :video_url, presence: true, :format => {:with => /\/\/(?:www\.)?youtu(?:\.be|be\.com)\/(?:watch\?v=|embed\/)?([a-z0-9_\-]+)/i}
+
+  def youtube_id
+    if self.video_url[/youtu\.be\/([^\?]*)/]
+      youtube_id = $1
+    else
+      self.video_url[/^.*((v\/)|(embed\/)|(watch\?))\??v?=?([^\&\?]*).*/]
+      youtube_id = $5
+    end
+    return youtube_id
+  end
 end

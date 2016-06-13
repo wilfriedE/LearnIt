2ec7357dbfe19f38d03fe2f2a88641d91c47a43a
Wilfried Hounyo <wilfried.hounyo@gmail.com>
2016-06-12 21:23:14 -0400

    added search to topics

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,18 @@
+class TopicsController < ApplicationController
+  def index
+    #if there is a search query, properly handle rendering them
+    if params[:q]
+      @topics = Topic.search(params[:q]).order("created_at DESC")
+    else
+      limit = 10
+      @topics = Topic.offset(params[:page].to_i * limit).first(limit)
+    end
+    respond_to do |format|
+      format.html
+      format.json { render json: @topics }
+    end
+  end
+
+  def show
+  end
+end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,12 @@
 class Topic < ActiveRecord::Base
   validates :name, presence: true
+
+  def self.search(search)
+    Topic.where("name LIKE ? OR description LIKE ?", "%#{search}%", "%#{search}%").distinct
+  end
+
+  def as_json(options={})
+    super(:only => [:id, :name, :description]
+    )
+  end
 end

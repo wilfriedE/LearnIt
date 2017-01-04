99aa062d4e7969a4d621947eb0b36b952f4c7664
Wilfried Hounyo <wilfried.hounyo@gmail.com>
2017-01-04 00:01:39 -0500

    Rails update

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_LearnIt_session'
+Rails.application.config.session_store :cookie_store, key: '_learn_it_session'

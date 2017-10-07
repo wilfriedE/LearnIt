0769c6245c1b03e09abb6168c62a35bfe3d7b550
Wilfried Hounyo <wilfried.hounyo@gmail.com>
2017-10-07 18:51:32 -0400

    Keep Topics clean for now

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,13 +1,3 @@
 class Topic < ApplicationRecord
-  has_many :topic_items, dependent: :destroy
-  has_many :courses, -> { distinct }, through: :topic_items, source: :topicable, source_type: 'Course'
-  has_many :tracks, -> { distinct },  through: :topic_items, source: :topicable, source_type: 'Track'
-  has_many :lesson_versions, -> { distinct }, through: :topic_items, source: :topicable, source_type: 'LessonVersion'
-  has_many :lessons, -> { distinct }, through: :lesson_versions
-  validates :name, presence: true, uniqueness: true
-
-  def as_json(_options = {})
-    super(only: %i[id name description])
-  end
-
+  # TODO: Add support for Topics. Temporariling not implementing to focus on other more critical parts.
 end
